8f8847e302b554673ec432b6c40824305a625820
Stephen Wills <stephenrwills94@gmail.com>
2022-07-07 12:00:06 -0500

    finished main method

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("What is your favorite subject in school?");
+
+string favSubject = Console.ReadLine();
+
+switch (favSubject)
+{
+    case "Math":
+    Console.WriteLine("Math is hard");
+    break;
+
+    case "English":
+        Console.WriteLine("English is boring");
+        break;
+
+    case "Science":
+        Console.WriteLine("Science is for dorks");
+        break;
+
+    case "History":
+        Console.WriteLine("History rules");
+        break;
+
+    case "Foreign Language":
+        Console.WriteLine("Foreign Language is interesting");
+        break;
+
+    default:
+        Console.WriteLine("This is not a required subject, Try Again");
+        break;
+
+}
